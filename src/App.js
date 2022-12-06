d2e31b65c731afa8604cd1f405daaaed9e3915db
vishal007p <107975264+vishal007p@users.noreply.github.com>
2022-12-06 19:32:36 +0530

    newe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Resturant from './compoent/basics/Resturant'
+import Todo from './compoent/todo/Todo';
  
 
 const App = () => {
-  return  <Resturant />;
+  return  <Todo />;
 }
 
 export default App;
